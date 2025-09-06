85e64d0b5e94bf4aa638f412ecec55bdc14739a6
Jose-sosa-82 <74826604+Jose-sosa-82@users.noreply.github.com>
2025-09-06 12:09:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh sosasjose.01@34.176.249.207 'cd /home/sosasjose.01/docker-gcp-deployment && git pull origin main && docker-compose up -d --build'
+cd /home/sosasjose.01/docker-gcp-deployment && git pull origin main && docker-compose up -d --build
